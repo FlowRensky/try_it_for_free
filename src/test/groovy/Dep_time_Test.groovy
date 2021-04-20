fdb1bfd5ef6b16f44a9c579c7cb524867eacfc42
FlowRensky <82861867+FlowRensky@users.noreply.github.com>
2021-04-20 15:35:42 +0500

    Create Dep_time_Test.groovy

---
+++ src/test/groovy/Dep_time_Test.groovy
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*
+import org.junit.Test
+import org.junit.Ignore
+import org.junit.Before
+import org.junit.After
+import org.junit.AfterClass
+import org.junit.BeforeClass
+
+
+class Dep_time_Test{
+    
+    @Test
+    void Simple_case(){
+        assertEquals(1,1)
+    }
+
+    @Test
+    void Case_simple(){
+        assertEquals(1,0)
+    }
+
+}
